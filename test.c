d9619eaf7ae4b25f3511728795bebef3675328fc
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-02-22 14:58:18 +0100

    add tests for cclist tail

--- test.c
+++ test.c
@@ -11,6 +11,7 @@ int main(void)
   for (int i = 1; i < NR_CELL_X * NR_CELL_Y; i++)
   {
     assert(cllist.head[i] == -1);
+    assert(cllist.tail[i] == -1);
   }
 
   Vec2 vec;
@@ -33,6 +34,10 @@ int main(void)
   assert(cllist.head[85] == 0);
   assert(cllist.head[68] == 1);
   assert(cllist.next[0] == 2);
+  assert(cllist.next[2] == 1);
+  assert(cllist.next[1] == -1);
+  assert(cllist.tail[85] == 2);
+  assert(cllist.tail[68] == 1);
 
   return 1;
 }
